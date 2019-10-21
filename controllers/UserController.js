26cbab01ff602c51a612930ce9b7b13d9fbb3380
talha915 <talhazafar915426@gmail.com>
2019-10-21 11:50:58 -0700

    All CRUD OPerations completed

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -25,4 +25,53 @@ router.post('/', function (req, res) {
     );
 });
 
+router.get('/', function (req, res) {
+    User.find({}, function (err, users) {
+        if (err) {
+            return res.status(500).send("There was a problem finding the users.");
+        }
+        else {
+            res.status(200).send(users);
+        }
+    });
+});
+
+router.get('/:id', function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) {
+            return res.status(500).send("There was a problem finding the user.");
+        }
+        if (!user) {
+            return res.status(404).send("No user found.");
+        }
+        else {
+            res.status(200).send(user);
+        }
+    });
+});
+
+router.delete('/:id', function (req, res) {
+    User.findByIdAndRemove(req.params.id, function (err, user) {
+        if (err) {
+            return res.status(500).send("There was a problem deleting the user.");
+        }
+        else {
+            res.status(200).send("User "+ user.name +" was deleted.");
+        }
+    });
+});
+
+
+router.put('/:id', function (req, res) {
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true},
+    function (err, user) {
+        if (err) {
+            return res.status(500).send("There was a problem updating the user."); 
+        }
+        else {
+            res.status(200).send(user);
+        }
+    });
+});
+
 module.exports = router;
